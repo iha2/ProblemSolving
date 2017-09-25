5ed9afb5d39599ab36dfcc763486a3fb921ec944
iheatu <iheatu@hubdoc.com>
2017-09-25 00:04:38 -0400

    adding questions above the answers as well as finishing off question 2

--- ProjectEuler/problem-1.hs
+++ ProjectEuler/problem-1.hs
@@ -1,4 +1,8 @@
 
+-- If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+
+-- Find the sum of all the multiples of 3 or 5 below 1000.
+
 
 isMultipleFromCollection :: (Integral a) => [a] -> a -> Bool
 isMultipleFromCollection [] value         = False
@@ -7,6 +11,7 @@ isMultipleFromCollection divisors value   =  foldr (\x -> (||) (value `mod` x ==
 sumOfMultiples :: (Integral a) => a -> [a] -> a
 sumOfMultiples n multiplesColl = sum $ filter (isMultipleFromCollection multiplesColl) [1..(n-1)]
 
+main :: IO ()
 main = do 
     let isFive = sumOfMultiples 1000 [3,5]
     print isFive
