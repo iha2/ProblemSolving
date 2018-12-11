9b406db70fcf2f5a9ade8ffe99a6677a8ee08242
iheatu <iheatudeveloper@gmail.com>
2018-12-10 21:08:39 -0500

    Advent of code problem one solved

--- AdventOfCode/src/problem1/main.cljs
+++ AdventOfCode/src/problem1/main.cljs
@@ -1,16 +1,18 @@
 (ns problem1.main (:require [cljs-node-io.core :as io :refer [aslurp aspit]]
-                            [cljs.core.async :refer [go chan <! >! buffer put!]]
+                            [cljs.core.async :refer [go chan <! >! buffer put! take!]]
                             [cljs.reader :refer [read-string]]))
 
 (defn read-from-file [file]
   (go
     (let [[err datastring] (<! (aslurp file))]
       (if-not err
-          (let [data (chan)]
-           (do
-             (put! data (read-string datastring))
-              data))
-          (println err)))))
+        (let [data (chan)]
+          (do
+            (let [info (read-string datastring)]
+              (put! data info)
+              println data)
+            data))
+        (println err)))))
 
 ; (defn get-problem-result []
 ;   (go 
@@ -21,4 +23,7 @@
   (reduce + 0 (apply vector a-result)))
 
 (defn main []
-  (go (println (read-from-file "public/data/data.edn"))))
+  (take!
+   (read-from-file "public/data/data.edn")
+   (fn [x] (go (let [result (<! x)]
+                 (println (get-result (apply vector result))))))))
