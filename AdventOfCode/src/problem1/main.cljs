b1ff74dfeaf031443b2b1b47b33225c73c2460e3
iheatu <iheatudeveloper@gmail.com>
2018-12-16 00:40:05 -0500

    answer to question 2 part 2

--- AdventOfCode/src/problem1/main.cljs
+++ AdventOfCode/src/problem1/main.cljs
@@ -4,26 +4,21 @@
 
 (defn read-from-file [file]
   (go
-    (let [[err datastring] (<! (aslurp file))]
+    (let [[err datastring] (<! (aslurp file))]s
       (if-not err
         (let [data (chan)]
-          (do
-            (let [info (read-string datastring)]
-              (put! data info)
-              println data)
+          (let [info (read-string datastring)]
+            (put! data info)
             data))
-        (println err)))))
+        (do err)))))
 
-; (defn get-problem-result []
-;   (go 
-;    (loop 
-;     (let (<! fil)))))
 
 (defn get-result [a-result]
   (reduce + 0 (apply vector a-result)))
 
 (defn main []
   (take!
-   (read-from-file "public/data/data.edn")
-   (fn [x] (go (let [result (<! x)]
-                 (println (get-result (apply vector result))))))))
+   (read-from-file "src/problem2/data.edn")
+   (fn [x] (go
+             (let [result (<! x)]
+               (println result))))))
---
+++ AdventOfCode/src/problem2/main.cljs
@@ -0,0 +1,42 @@
+(ns problem2.main (:require [cljs-node-io.core :as io :refer [aslurp aspit]]
+                            [cljs.core.async :refer [go chan <! >! buffer put! take!]]
+                            [cljs.reader :refer [read-string]]
+                            [clojure.string :refer [split]]))
+
+(defn read-from-file [file]
+  (go
+    (let [[err datastring] (<! (aslurp file))]
+      (if-not err
+        (let [data (chan)]
+          (let [info (read-string datastring)]
+            (put! data info)
+            data))
+        (println err)))))
+
+(defn meta-string [raw-string]
+  "gets how many characters per type {:character -number- }"
+  (loop [characters (split raw-string "")
+         string-meta {}]
+    (let [character-key (keyword (first characters))]
+      (cond (empty? characters) string-meta
+            (->> character-key string-meta nil?) (recur (rest characters) (assoc string-meta character-key 1))
+            :else (recur (rest characters) (update string-meta character-key inc))))))
+
+(defn key-values-in-range [min max object]
+  "gets how many keys have a value in the range"
+  (let [obj-values (->> object vals set)]
+    (reduce #(if (contains? obj-values %2) (conj %1 %2) %1) #{} (range min max))))
+
+(defn checksum-term-value [x] (key-values-in-range 2 4 x))
+
+(defn checksum-term-sumation [all-terms-values]
+  (reduce #(let [value (key-values-in-range 2 4 (meta-string %2))]
+             {:2 (+ (:2 %1) (if (contains? value 2) 1 0))
+              :3 (+ (:3 %1) (if (contains? value 3) 1 0))}) {:2 0 :3 0} (apply vector all-terms-values)))
+(defn main []
+  (take!
+   (read-from-file "src/problem2/data.edn")
+   (fn [x] (go
+             (let [result (<! x)]
+               (let [answer (checksum-term-sumation result)]
+                 (println (* (:2 answer) (:3 answer)))))))))
