ae26f77120459d0bde31d322e48e216676ceb065
iheatu <iheatudeveloper@gmail.com>
2018-12-19 02:45:19 -0500

    working version of problem 1

--- AdventOfCode/src/problem1/main.cljs
+++ AdventOfCode/src/problem1/main.cljs
@@ -4,21 +4,31 @@
 
 (defn read-from-file [file]
   (go
-    (let [[err datastring] (<! (aslurp file))]s
-      (if-not err
-        (let [data (chan)]
-          (let [info (read-string datastring)]
-            (put! data info)
-            data))
-        (do err)))))
+    (let [[err datastring] (<! (aslurp file))] s
+         (if-not err
+           (let [data (chan)]
+             (let [info (read-string datastring)]
+               (put! data info)
+               data))
+           (do err)))))
 
 
-(defn get-result [a-result]
+(defn resulting-frequency [a-result]
   (reduce + 0 (apply vector a-result)))
+
+(defn duplicate-frequency [collection]
+  (loop [coll collection
+         last-freq 0
+         frequencies #{}]
+    (let [current-freq (+ last-freq (first coll))]
+      (if (contains? frequencies current-freq)
+        current-freq
+        (recur (rest coll) current-freq (conj frequencies current-freq))))))
 
 (defn main []
   (take!
-   (read-from-file "src/problem2/data.edn")
+   (read-from-file "src/problem1/data.edn")
    (fn [x] (go
              (let [result (<! x)]
-               (println result))))))
+               (println (resulting-frequency (apply vector result)))
+               (println (duplicate-frequency (cycle (apply vector result)))))))))
