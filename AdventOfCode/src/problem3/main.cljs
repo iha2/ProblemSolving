9aad1959bf0fe53f969cb779db63bc20a1f38c1b
iheatu <iheatudeveloper@gmail.com>
2018-12-20 08:19:49 -0500

    almost finished question 3 part 2

--- AdventOfCode/src/problem3/main.cljs
+++ AdventOfCode/src/problem3/main.cljs
@@ -2,7 +2,7 @@
                             [cljs.core.async :refer [go chan <! >! buffer put! take!]]
                             [cljs.reader :refer [read-string]]))
 
-(defn construct-cube [properties]
+(defn construct-rec [properties]
   {:index (nth properties 0)
    :meta-data (nth properties 1)
    :top-left-x (nth properties 2)
@@ -16,21 +16,34 @@
         unique-key (keyword (str new-x "-" new-y))]
     {unique-key 1}))
 
-(defn update-grid-with-cube [grid cube]
+(defn update-grid-with-rec [grid rec]
   (reduce (fn [update-grid-y height]
-            (let [top-left-x (:top-left-x cube)
-                  top-left-y (:top-left-y cube)
-                  width-vals (range (:width cube))]
+            (let [top-left-x (:top-left-x rec)
+                  top-left-y (:top-left-y rec)
+                  width-vals (range (:width rec))]
               (reduce (fn [update-grid-x width]
                         (let [grid-value (create-grid-unit top-left-y top-left-x width height)]
                           (if (nil? ((-> grid-value keys first) update-grid-x)) nil ((-> grid-value keys first) update-grid-x))
                           (merge-with + update-grid-x grid-value)))
                       update-grid-y width-vals)))
-          grid (range (:height cube))))
+          grid (range (:height rec))))
 
 (defn generate-grid [grid-data]
-  (reduce #(update-grid-with-cube %1 %2) {} grid-data))
+  (reduce #(update-grid-with-rec %1 %2) {} grid-data))
 
+(defn part1 [claims]
+  (count (filter #(> (second %1) 1) (->> claims generate-grid))))
+
+(defn get-corners-of-rec [rec]
+  {:top-left {:x (:top-left-x rec) :y (:top-right-y rec)}
+   :top-right {:x (+ (:width rec) (:top-left-x rec)) :y (:top-right-y rec)}})
+
+(defn get-corners-of-rec [rec]
+  {:top-left {:x (:top-left-x rec) :y (:top-right-y rec)}
+   :top-right {:x (+ (:width rec) (:top-left-x rec)) :y (:top-right-y rec)}
+   :bottom-left {:x (:top-left-x rec) :y (+ (:height rec) height)
+                 }
+   :bottom-right {:x (+ (:width rec) (:top-left-x rec)) :y (+ (+ (:width rec) (:top-left-y rec)))}})
 
 
 (defn read-from-file [file]
@@ -50,5 +63,10 @@
    (read-from-file "src/problem3/data.edn")
    (fn [x] (go (let [result (<! x)]
                  (let [cljs-answer (apply vector result)]
-                  ;  (println (merge-with +  {:a 12 :b 12} {:a 12 :b 12}) )
-                   (println (count (filter #(> (second %1) 1) (->> (map construct-cube cljs-answer) generate-grid))))))))))
+                   (let [claims (->> (map construct-rec cljs-answer))
+                         grided-claims (part1 claims)]
+                     (println (filter #(let [bottom-right (keyword (str (+ (:width %1) (:top-left-x %1)) "-" (+ (:height %1) (:top-left-y %1))))
+                                             bottom-left (keyword (str (:top-left-x %1) "-" (+ (:height %1) (:top-left-y %1))))
+                                             top-right (keyword (str (+ (:width %1) (:top-left-x %1)) "-" (:top-left-y %1)))]
+                                         (if (nil? (bottom-right grided-claims))
+                                           false)) claims)))))))))
