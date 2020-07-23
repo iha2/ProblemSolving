d5f0f1bfa90a678b16676afd75b4015220ae9207
Iheatu Wogu <iheatuwogu@Iheatus-MacBook-Pro.local>
2020-07-22 21:50:05 -0400

    better javascript solution

--- CrackTheCodingInterview/javascript/src/chapter-one/one-point-three.js
+++ CrackTheCodingInterview/javascript/src/chapter-one/one-point-three.js
@@ -1,34 +1,23 @@
-// 1.3 Given 2 string, check if one is a permutation of the other
+// Given 2 string check if one is a permutation of the other
 // normal larmon
 
-const checkPermutation = (firstString, secondString) => {
-  const firstStringCharacters = firstString.split("");
-  const secondStringCharacters = secondString.split("");
+const stringToValue = (string) => {
+  const stringCharacters = string.split("");
+  return stringCharacters.reduce((stringValue, character) => {
+    return stringValue + character.charCodeAt(0);
+  }, 0);
+};
 
-  if (secondStringCharacters.length !== firstStringCharacters.length) {
+const checkPermutation = (firstString, secondString) => {
+  if (firstString.length !== secondString.length) {
     return false;
   } else {
-    const resultMap = firstStringCharacters.reduce((result, character) => {
-      if (result[character]) {
-        return { ...result, [character]: result[character] + 1 };
-      } else {
-        return { ...result, [character]: 1 };
-      }
-    }, {});
-
-    const nextResultMap = secondStringCharacters.reduce((result, character) => {
-      if (result[character]) {
-        return { ...result, [character]: result[character] + 1 };
-      } else {
-        return { ...result, [character]: 1 };
-      }
-    }, {});
-
-    return Object.keys(nextResultMap).reduce((result, key) => {
-      return result && !!nextResultMap[key] && !!resultMap[key];
-    }, true);
+    const firstStringValue = stringToValue(firstString);
+    const secondStringValue = stringToValue(secondString);
+    return firstStringValue === secondStringValue;
   }
 };
 
-console.log(checkPermutation("larmon", "normal"));
-console.log(checkPermutation("wweerrtt", "wewertrt"));
+console.log(checkPermutation("normal", "larmon"));
+console.log(checkPermutation("lester", "reestl"));
+console.log(checkPermutation("leszter", "reestl"));
