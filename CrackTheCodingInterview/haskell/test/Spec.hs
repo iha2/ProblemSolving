d67370213d76bb42a710596d444faf9d733c57f2
Iheatu Wogu <iheatuwogu@Iheatus-MacBook-Pro.local>
2020-07-26 17:37:01 -0400

    added some more test regarding string compressor

--- CrackTheCodingInterview/haskell/test/Spec.hs
+++ CrackTheCodingInterview/haskell/test/Spec.hs
@@ -1,11 +1,17 @@
 import Chapter1.OnePointThree ( isPermutation )
+import Chapter1.OnePointFive ( stringCompressor )
 import Test.HUnit
 
 
 isPalindromeTest =
   TestCase $ assertBool "should be able to catch permutation normal and larmon" $ isPermutation "normal" "larmon"
 
+shouldCompressAString = TestCase $ assertEqual "should produce a compressed string" (stringCompressor "qweqweeqweeeeertgggggggggggggggggggggfdsa") "a1s1d1f1g21t1r1e5w1q1e2w1q1e1w1q1" 
+
+shouldReturnTheSameString = TestCase $ assertEqual "should return the same string" (stringCompressor "qweqweeqweeeeertgggggggggggggfdsa") "qweqweeqweeeeertgggggggggggggfdsa"
+
 main :: IO ()
 main = do
-  runTestTT $ TestList [isPalindromeTest]
+  runTestTT $ TestList [isPalindromeTest, shouldCompressAString, shouldReturnTheSameString]
   return ()
+
