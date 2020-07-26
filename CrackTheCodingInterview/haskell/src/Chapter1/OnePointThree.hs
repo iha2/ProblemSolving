2f97e503afd587298a4e103a3aed563536a1e39e
Iheatu Wogu <iheatuwogu@Iheatus-MacBook-Pro.local>
2020-07-26 19:35:52 -0400

    redo string hash

--- CrackTheCodingInterview/haskell/src/Chapter1/OnePointThree.hs
+++ CrackTheCodingInterview/haskell/src/Chapter1/OnePointThree.hs
@@ -1,8 +1,8 @@
 {-# LANGUAGE OverloadedStrings #-}
 
 module Chapter1.OnePointThree
-  ( stringToMap,
-    isPermutation
+  ( stringToMap
+  , isPermutation
   )
 where
 
@@ -12,22 +12,15 @@ where
 import qualified Data.Text                     as T
 import qualified Data.Map                      as M
 import           Data.Maybe
+import           Data.Char
 
-stringToMap :: M.Map Char Int -> String -> M.Map Char Int
-stringToMap stringMap [] = stringMap
-stringToMap stringMap (character : characters) =
-  if Data.Maybe.isJust $ M.lookup character stringMap
-    then stringToMap (M.insertWith (+) character 1 stringMap) characters
-    else stringToMap (M.insert character 1 stringMap) characters
 
 stringHash :: String -> Int
-stringHash string =
-  foldr (\x y -> (fromEnum (fst x) * snd x) + y) 0 $ M.toList $ stringToMap
-    M.empty
-    string
+stringHash  =
+  foldl (\hashValue character -> ord character + hashValue) 0 
+
 
 isPermutation :: String -> String -> Bool
-isPermutation firstString secondString =
-  if length firstString == length secondString
-    then stringHash firstString == stringHash secondString
-    else False
+isPermutation firstString secondString = stringHash firstString == stringHash secondString
+
+
